24b33b4e65abb2961e8f7085eb7dd214c7d14f14
David Vescovi <dvescovi@tampabay.rr.com>
2025-12-16 11:24:55 -0500

    add 563 remove more targets

---
+++ etc/stm32cubeh5.mk
@@ -0,0 +1,13 @@
+include $(OPENMRNPATH)/etc/path.mk
+
+ifdef STM32CUBEH5PATH
+INCLUDES += -I$(OPENMRNPATH)/src/freertos_drivers/st \
+            -I$(STM32CUBEH5PATH)/Drivers/STM32H5xx_HAL_Driver/Inc \
+            -I$(STM32CUBEH5PATH)/Drivers/CMSIS/Device/ST/STM32H5xx/Include \
+            -I$(STM32CUBEH5PATH)/Drivers/CMSIS/Include
+endif
+
+CFLAGS += 
+CXXFLAGS += 
+
+DEPS += STM32CUBEH5PATH
