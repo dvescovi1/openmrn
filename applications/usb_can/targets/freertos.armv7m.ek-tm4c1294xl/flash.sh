b8801fbe414e61f35c66d7fc052ffa213e736bde
David Vescovi <dvescovi@tampabay.rr.com>
2025-12-16 14:22:54 -0500

    removed app targets

--- applications/usb_can/targets/freertos.armv7m.ek-tm4c1294xl/flash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/Applications/Energia.app/Contents/Resources/Java/hardware/tools/lm4f/bin/arm-none-eabi-objcopy -O binary usb_can.elf usb_can.bin
-/Applications/Energia.app/Contents/Resources/Java/hardware/tools/lm4f/bin/lm4flash -v usb_can.bin
