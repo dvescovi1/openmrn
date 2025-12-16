ae80856ec5223cd6680fb404c286bfbea2a0aea4
David Vescovi <dvescovi@tampabay.rr.com>
2025-12-16 17:23:25 -0500

    freertos includes

---
+++ inc/freertos_includes.h
@@ -0,0 +1,9 @@
+#ifndef FREERTOS_INCLUDES_H
+#define FREERTOS_INCLUDES_H
+
+#include "semphr.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+#include "event_groups.h"
+#endif // FREERTOS_INCLUDES_H
