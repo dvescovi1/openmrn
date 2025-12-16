f8de0b3b8f13d0c71e74f3472e9d4ec2de0f70f8
David Vescovi <dvescovi@tampabay.rr.com>
2025-12-16 06:57:09 -0500

    remove 1

--- bin/reformat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find . -name *.h -o -name *.cxx -o -name *.hxx -o -name *.c | xargs clang-format-3.4 -i -style=file
--- etc/nonos.xtensa-lx106.esp8266.mk
+++
@@ -1,8 +0,0 @@
-
-include $(OPENMRNPATH)/etc/xtensa-lx106.mk
-
-INCLUDES += -I$(OPENMRNPATH)/inc/esp8266  \
-
-SYSLIB_SUBDIRS += spiffs core
-SYSLIBRARIES += -lspiffs -lcore
-
