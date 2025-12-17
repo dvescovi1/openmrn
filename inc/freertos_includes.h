a4b4d2fcaf629fc044cf10883e47668f8fd25ad8
David Vescovi <dvescovi@tampabay.rr.com>
2025-12-17 06:19:45 -0500

    freertos headers

--- inc/freertos_includes.h
+++ inc/freertos_includes.h
@@ -1,9 +1,9 @@
 #ifndef FREERTOS_INCLUDES_H
 #define FREERTOS_INCLUDES_H
 
-#include "semphr.h"
-#include "task.h"
-#include "queue.h"
-#include "timers.h"
-#include "event_groups.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/timers.h"
+#include "freertos/event_groups.h"
 #endif // FREERTOS_INCLUDES_H
